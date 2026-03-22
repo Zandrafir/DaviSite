f235421077e4eb2080fedc590ac44265673edf9c
unknown <tribeiro.santos@outlook.com>
2026-03-22 19:44:43 -0300

    Adicionando pasta img

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 module.exports = function(eleventyConfig) {
-  // Copia a pasta admin para o site final
+  // 1. Copia a pasta do painel administrativo
   eleventyConfig.addPassthroughCopy("admin");
-  
-  // Copia as imagens para não sumirem
+
+  // 2. Copia a pasta assets (onde estarão suas fotos)
   eleventyConfig.addPassthroughCopy("assets");
+
+  // 3. Garante a cópia de imagens que possam estar soltas
   eleventyConfig.addPassthroughCopy("*.png");
   eleventyConfig.addPassthroughCopy("*.jpg");
+  eleventyConfig.addPassthroughCopy("*.jpeg");
+  eleventyConfig.addPassthroughCopy("*.gif");
 
   return {
     dir: {
